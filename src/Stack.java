020cecfb0e462453864dcef037fd5f1294b71222
Vinit Rathaur <vinitrathaur9149@gmail.com>
2023-01-18 12:54:59 +0530

    Initial commit

---
+++ src/Stack.java
@@ -0,0 +1,25 @@
+public class Stack {
+    int[] arr;
+    int top;
+    Stack(int capacity){
+        arr = new int[capacity];
+        top=0;
+
+    }
+    Stack(){
+        this(20);
+    }
+//to push a value
+    public void push(int data){
+        arr[top++]=data;
+    }
+    // the size
+    public boolean isfull(){
+        return top ==arr.length;
+    }
+
+    public static void main(String[] args) {
+        Stack st = new Stack();
+        st.push(20);
+    }
+}
