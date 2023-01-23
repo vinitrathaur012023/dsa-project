4e75fc8a80986d7ec7c90d3dfac090ed9ad4c277
Vinit Rathaur <vinitrathaur9149@gmail.com>
2023-01-23 12:31:57 +0530

    to print a smallest number in java

--- src/Largest.java
+++ src/Largest.java
@@ -3,11 +3,11 @@ import java.util.*;
 public class Largest {
     public static void main(String[] args) {
         int []arr=new int[] {40,30,50,80};
-        int max=arr[0];
+        int min=arr[0];
         for (int i=0; i<arr.length; i++){
-     if(arr[i]>max)
-         max=arr[i];
+     if(arr[i]<min)
+         min=arr[i];
         }
-        System.out.println(max);
+        System.out.println(min);
     }
 }
